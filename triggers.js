144bd069127f774e65e113cd0b702b39a1b64e9d
Menno van Grinsven <scanno71@gmail.com>
2017-08-14 22:34:17 +0200

    Update MQTT to latest npm version

--- triggers.js
+++ triggers.js
@@ -1,6 +1,10 @@
-var broker    = require("./broker.js");
+const Homey     = require('homey');
+
+const broker    = require("./broker.js");
 var globalVar = require("./global.js");
 var logmodule = require("./logmodule.js");
+
+var eventMQTT = null;
 
 module.exports = {
    getTriggerArgs: function() {
@@ -11,51 +15,80 @@ module.exports = {
    },
    setArgumentChangeEvent: function() {
       setArgumentChangeEvent();
+   },
+   getEvenMQTT: function() {
+      return eventMQTT;
    }
 }
 
 
 function listenForMessage () {
    // Start listening for the events.
-   Homey.manager('flow').on('trigger.eventMQTT', processMessage);
+   eventMQTT = new Homey.FlowCardTrigger('eventMQTT');
+   eventMQTT.register();
+
+   eventMQTT.registerRunListener((args, state ) => {
+      logmodule.writelog('info', "Listener eventMQTT called");
+      try {
+         if ( processMessage(args, state)) {
+            return Promise.resolve( true );
+         } else {
+            return Promise.resolve( false );
+         }
+       } catch(err) {	
+         logmodule.writelog('error', "Error in Listener eventMQTT: " +err);
+         return Promise.reject(err);
+       }
+   })
+
 }
 
 
-function getTriggerArgs() {
-   return new Promise(function (fulfill, reject) {
+getTriggerArgs = async() => {
+   try {
       if (globalVar.getTopicArray().length > 0) {
-        globalVar.clearTopicArray();
+         globalVar.clearTopicArray();
       };
-      logmodule.writelog("Registered topics:" + globalVar.getTopicArray());
-      return getEventMQTTArgs().then(function() {
-         logmodule.writelog("Registered topics:" + globalVar.getTopicArray());
-         fulfill(true);
+      logmodule.writelog('info', "Registered topics:" + globalVar.getTopicArray());
+      await getEventMQTTArgs().then((result) => {
+         logmodule.writelog('info', "Registered topics:" + globalVar.getTopicArray());
+         return true;
       });
-   });
+   } catch(err) {
+      logmodule.writelog('error', "getTriggerArgs: "+err);
+      return err;
+   }
 }
 
-function getEventMQTTArgs() {
-   return new Promise(function (fulfill, reject) {
-      Homey.manager('flow').getTriggerArgs('eventMQTT', function( err, args ) {
-         args.forEach(function(element) {
-            logmodule.writelog("Trigger Arguments for eventMQTT: " + element.mqttTopic);
-            broker.subscribeToTopic(element.mqttTopic);
+getEventMQTTArgs = async() => {
+//   return new Promise(function (fulfill, reject) {
+   try {
+      await eventMQTT.getArgumentValues(function( err, values ) {
+         values.forEach(function(element) {
+            logmodule.writelog('info', "Trigger Arguments for eventMQTT: " + element.mqttTopic);
+//            broker.subscribeToTopic(element.mqttTopic);
+              require("./broker.js").subscribeToTopic(element.mqttTopic);
          });
-         fulfill(true);
+         logmodule.writelog('info', "boe");
+         return true;
       });
-   });
+   } catch(err) {
+      logmodule.writelog('error', "getEventMQTTArgs: " +err);
+      return err;
+   }
+//   });
 }
 
 
-function processMessage (callback, args, state) {
+function processMessage (args, state) {
    var reconnectClient = false;
 
    // Make a connection to the broker. But only do this once. When the app is started, the connectedClient
    // variable is set to null, so there is no client connection yet to the broker. If so, then connect to the broker.
    // Otherwise, skip the connection.
-   broker.connectToBroker(args, state);
+   require("./broker.js").connectToBroker(args, state);
 
-   logmodule.writelog ("state.topic = " + state.triggerTopic + " topic = " + args.mqttTopic)
+   logmodule.writelog ('info', "state.topic = " + state.triggerTopic + " topic = " + args.mqttTopic)
 
    // MQTT subscription topics can contain "wildcards", i.e a + sign. However the topic returned
    // by MQTT brokers contain the topic where the message is posted on. In that topic, the wildcard
@@ -78,14 +111,14 @@ function processMessage (callback, args, state) {
    // If the topic that triggered me the topic I was waiting for?
    if (matchTopic == true) {
       console.log ("triggerTopic = equal" )
-      callback( null, true )
+      return true;
    }
    // This is not the topic I was waiting for and it is a known topic
    else if (state.triggerTopic !== args.mqttTopic & globalVar.getTopicArray().indexOf(args.mqttTopic) !== -1) {
-      logmodule.writelog("We are not waiting for this topic");
-      callback( null, false )
+      logmodule.writelog('info', "We are not waiting for this topic");
+      return false;
    };
-   callback (null, false);
+   return false;
 }
 
 
@@ -93,29 +126,30 @@ function setArgumentChangeEvent() {
    // We need to know when an argument in a trigger has changed, has been added or removed.
    // If so, we need to change, remove or add topic subscriptions. So register to the 
    // trigger update event.
-   Homey.manager('flow').on('trigger.eventMQTT.update', function( callback ) {
-      logmodule.writelog("Trigger changed" );
+   eventMQTT.on('update', () => {
+//   Homey.ManagerFlow.update('eventMQTT', function( callback ) {
+      logmodule.writelog('info', "Trigger changed" );
 
       // get the new arguments
-      Homey.manager('flow').getTriggerArgs('eventMQTT', function( err, args ) {
-         logmodule.writelog("topics:" + globalVar.getTopicArray());
+      eventMQTT.getArgumentValues(function( err, values ) {
+         logmodule.writelog('info', "topics:" + globalVar.getTopicArray());
 
          // Check if there are already subscribed topics. If so, then unsubsribe because we 
          // need to loop through all triggers and just unsubscribe and re-subscribe again is faster.
          if (globalVar.getTopicArray().length > 0) {
-            broker.getConnectedClient().unsubscribe(globalVar.getTopicArray());
+            require("./broker.js").getConnectedClient().unsubscribe(globalVar.getTopicArray());
             globalVar.clearTopicArray();
          };
          // `args` is an array of trigger objects, one entry per flow
-         args.forEach(function(element) {
-            logmodule.writelog("Trigger Arguments: " + element.mqttTopic);
-            broker.subscribeToTopic(element.mqttTopic);
+         values.forEach(function(element) {
+            logmodule.writelog('info', "Trigger Arguments: " + element.mqttTopic);
+            require("./broker.js").subscribeToTopic(element.mqttTopic);
          });
-         logmodule.writelog("topics:" + globalVar.getTopicArray());
+         logmodule.writelog('info', "topics:" + globalVar.getTopicArray());
       });
 
       // always fire the callback, it's reserved for future argument validation
-      callback( null, true );
+    //  callback( null, true );
    });
 }
 
