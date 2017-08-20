ed70f0cc27be92a60c7ee14c8d89668dcd0cd12e
Menno van Grinsven <scanno71@gmail.com>
2017-08-20 23:12:48 +0200

    Convert to classes: message handling

--- messagehandling.js
+++ messagehandling.js
@@ -1,29 +1,41 @@
 "use strict";
-const Homey = require('homey');
+//const Homey = require('homey');
 
-var globalVar = require("./global.js");
-var logmodule = require("./logmodule.js");
-var triggers  = require("./triggers.js");
 
-module.exports = {
+/*module.exports = {
    receiveMessage: function(topic, message, args, state) {
       receiveMessage(topic, message, args, state);
    }
 }
+*/
 
-function receiveMessage(topic, message, args, state) {
-   var validJSON = true;
-   logmodule.writelog('info', "received '" + message.toString() + "' on '" + topic + "'");
+class handlingMQTT {
 
-   let tokens = {
-      message: message.toString()
+   constructor(app) {
+      this.globalVar = app.globalVar;
+      this.logmodule = app.logmodule;
+      this.triggers  = app.triggers;
    }
    
-   let triggerstate = {
-      triggerTopic: topic, 
+   receiveMessage(topic, message, args, state) {
+      var validJSON = true;
+      this.logmodule.writelog('info', "received '" + message.toString() + "' on '" + topic + "'");
+
+      let tokens = {
+         message: message.toString()
+      }
+   
+      let triggerstate = {
+         triggerTopic: topic, 
+      }
+   
+      this.triggers.getEventMQTT().trigger(tokens, triggerstate, null);
+      this.logmodule.writelog('info', "Trigger generic card for " + topic);
    }
    
-   triggers.getEvenMQTT().trigger(tokens, triggerstate, null)
-//   Homey.manager('flow').trigger('eventMQTT', { message: message.toString() }, { triggerTopic: topic });
-   logmodule.writelog('info', "Trigger generic card for " + topic);
+   updateRef(app) {
+      this.triggers = app.triggers;
+   }
 }
+
+module.exports = handlingMQTT;
