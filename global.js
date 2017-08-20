ed7ff967056ef5b7b9366651ebe40afd4b54446c
Menno van Grinsven <scanno71@gmail.com>
2017-08-20 23:12:15 +0200

    Convert to classes: global

--- global.js
+++ global.js
@@ -1,45 +1,40 @@
+"use strict";	
 
-var topicArray = [];
+class globalVars {
 
-var logmodule = require("./logmodule.js");
-
-module.exports = {
-   getTopic: function(topicName) {
-      return getTopic(topicName);
-   },
-   setTopic: function(topicData) {
-      setTopic(topicData);
-   },
-   getTopicArray: function() {
-      return topicArray;
-   },
-   clearTopicArray: function() {
-      clearTopicArray();
+   constructor(app) {
+//     if (this.topicArray == undefined) {
+         this.topicArray = [];
+//      }
+//      this.logmodule = require("./logmodule.js");
    }
-}
 
-function getTopic(topicName) {
-   for (var i=0; i < topicArray.length; i++) {
-      if (topicArray[i].topicName === topicName) {
-         return topicArray[i];
+   getTopicArray() {
+      return this.topicArray;
+   }
+   
+   getTopic(topicName) {
+      for (var i=0; i < this.topicArray.length; i++) {
+         if (this.topicArray[i].topicName === topicName) {
+            return this.topicArray[i];
+         }
+      }
+      // Topic has not been found, so return null
+      return null
+   }
+
+   setTopic(topicData) {
+      var entryArray = getTopic(topicData.topicName);
+      if (entryArray !== null) {
+         entryArray = topicData;
+      } else {
+         // User has not been found, so assume this is a new user
+         this.topicArray.push(topicData);
       }
    }
-   // Topic has not been found, so return null
-   return null
-}
 
-function setTopic(topicData) {
-   var entryArray = getTopic(topicData.topicName);
-   if (entryArray !== null) {
-      entryArray = topicData;
-      
-   } else {
-      // User has not been found, so assume this is a new user
-      topicArray.push(topicData);
+   clearTopicArray() {
+      this.topicArray = [];
    }
 }
-
-function clearTopicArray() {
-   topicArray = [];
-}
-
+module.exports = globalVars;
