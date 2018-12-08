cfcb630e2f3fceb853dc45a2a16981c197e93c4b
Menno van Grinsven <scanno71@gmail.com>
2018-12-08 14:35:47 +0100

    mqtt: Some cleanup and jsdoc

--- messagehandling.js
+++ messagehandling.js
@@ -1,13 +1,4 @@
 "use strict";
-//const Homey = require('homey');
-
-
-/*module.exports = {
-   receiveMessage: function(topic, message, args, state) {
-      receiveMessage(topic, message, args, state);
-   }
-}
-*/
 
 class handlingMQTT {
 
@@ -17,6 +8,17 @@ class handlingMQTT {
       this.triggers  = app.triggers;
    }
 
+   /**
+    * receiveMessage - Handling of received messages.
+    * This gets called as soon as the client receives a message published
+    * on a subscribed topic
+    *
+    * @param  {type} topic   topic where message was published on
+    * @param  {type} message payload of the received message
+    * @param  {type} args    list of arguments that are part of the trigger FlowCardTrigger
+    * @param  {type} state   state arguments of the FlowCardTrigger
+    * @return {type}         no return value
+    */
    receiveMessage(topic, message, args, state) {
       var validJSON = true;
       const ref=this;
@@ -37,6 +39,12 @@ class handlingMQTT {
       this.logmodule.writelog('info', "Trigger generic card for " + topic);
    }
 
+   /**
+    * updateRef - updates references to other classes
+    *
+    * @param  {type} app reference to the main app instance
+    * @return {type}     no return value
+    */
    updateRef(app) {
       this.triggers = app.triggers;
    }
