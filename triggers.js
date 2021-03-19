ffe5bdb91873c0b10aa7c9fa662c6b390a807945
Menno van Grinsven <scanno71@gmail.com>
2021-03-19 22:12:19 +0100

    Use matching logic from mqtt-wildcard

--- triggers.js
+++ triggers.js
@@ -72,7 +72,7 @@ class triggerMQTT {
       }
   }
 
-  topicMatches(topic, pattern) {
+  topicMatches(topic, wildcard) {
     // MQTT subscription topics can contain "wildcards", i.e a + sign. However the topic returned
     // by MQTT brokers contain the topic where the message is posted on. In that topic, the wildcard
     // is replaced by the actual value. So we will have to take into account any wildcards when matching the topics.
@@ -80,14 +80,38 @@ class triggerMQTT {
     // by MQTT brokers contain the topic where the message is posted on. In that topic, the wildcard
     // is replaced by the actual value. So we will have to take into account any wildcards when matching the topics.
 
-    this.logmodule.writelog ('info', "topic: "+topic+" pattern: "+pattern);
-    const regex = new RegExp('^' + pattern.replace(/\+/, '[^/]+').replace(/#/, '.+') + '$');
-    var result = regex.test(topic);
-    this.logmodule.writelog ('info', "regex test result: "+result);
-    return result;
+    this.logmodule.writelog ('info', "topic: "+topic+" pattern: "+wildcard);
+    if (topic === wildcard) {
+        return true;
+    } else if (wildcard === '#') {
+        return true;
+    }
+    var res = [];
+    var t = String(topic).split('/');
+    var w = String(wildcard).split('/');
+
+    var i = 0;
+    for (var lt = t.length; i < lt; i++) {
+        if (w[i] === '+') {
+            res.push(t[i]);
+        } else if (w[i] === '#') {
+            res.push(t.slice(i).join('/'));
+            return true;
+        } else if (w[i] !== t[i]) {
+            return false;
+        }
+    }
+
+    if (w[i] === '#') {
+        i += 1;
+    }
+
+    if (i === w.length) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
-
    processMessage(args, state) {
       var reconnectClient = false;
 
