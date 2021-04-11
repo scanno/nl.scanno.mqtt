6706d95272ff5e2af187590f03a1f94c998160e9
Harrie de Groot <harriedegroot@gmail.com>
2021-04-11 23:23:30 +0200

    Flow card triggers: - prevent destruction of the full topics array, causing all api connections being lost - subscribe to all flow card trigger topics on initialisation - on trigger card change (flow card config): unsubscribe from removed topics & subscribe to newly added topics - await the Homey trigger call - check for api topic match before forwarding the message to the realtime api

--- messagehandling.js
+++ messagehandling.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const { ManagerApi } = require('homey');
-
 class handlingMQTT {
 
    constructor(app) {
@@ -16,18 +14,22 @@ class handlingMQTT {
     * on a subscribed topic
     *
     * @param  {type} topic   topic where message was published on
-    * @param  {type} message payload of the received message
+    * @param  {type} buffer  payload of the received message
     * @param  {type} args    list of arguments that are part of the trigger FlowCardTrigger
     * @param  {type} state   state arguments of the FlowCardTrigger
     * @return {type}         no return value
     */
-   receiveMessage(topic, message, args, state) {
-      var validJSON = true;
-      const ref=this;
-      this.logmodule.writelog('info', "received '" + message.toString() + "' on '" + topic + "'");
+   async receiveMessage(topic, buffer) {
+      this.logmodule.writelog('info', "received '" + buffer.toString() + "' on '" + topic + "'");
 
+      const message = buffer.toString();
+      await this.callTriggers(topic, message)
+      this.dispatchToRealtimeApi(topic, message);
+   }
+
+   async callTriggers(topic, message){
       let tokens = {
-         message: message.toString(),
+         message: message,
          topic: topic
       }
 
@@ -35,26 +37,32 @@ class handlingMQTT {
          triggerTopic: topic,
       }
 
-      ref.triggers.getEventMQTT().trigger(tokens, triggerstate, null).catch( function(e) {
-        ref.logmodule.writelog('error', "Error occured: " +e);
-      })
-      this.logmodule.writelog('info', "Trigger generic card for " + topic);
 
-      this.dispatchToRealtimeApi(topic, message);
+      try {
+         this.logmodule.writelog('debug', "Trigger generic card for " + topic);
+         await this.triggers.getEventMQTT().trigger(tokens, triggerstate);
+      } catch(e) {
+         this.logmodule.writelog('error', "Error occured: " +e);
+      }
    }
 
-    dispatchToRealtimeApi(topic, message) {
-        var messageToSend;
-        this.logmodule.writelog('debug', "send message to listeners via realtime api");
-        this.logmodule.writelog('debug', topic + ": " + message.toString());
-        try {
-            messageToSend = JSON.parse(message.toString());
-        } catch (err) {
-            messageToSend = message.toString();
-        }
-        //ManagerApi.realtime(topic, messageToSend);
-        this.Homey.api.realtime(topic, messageToSend);
-    }
+   dispatchToRealtimeApi(topic, message) {
+      var messageToSend;
+      try {
+         messageToSend = JSON.parse(message);
+      } catch (err) {
+         messageToSend = message;
+      }
+
+      let apiTopics = this.triggers.broker.getTopicArray().getApiTopics();
+      if(apiTopics.some(wildcard => this.triggers.topicMatches(topic, wildcard))){
+         this.logmodule.writelog('debug', "send message to listeners via realtime api");
+         this.logmodule.writelog('debug', topic + ": " + message);
+         this.Homey.api.realtime(topic, messageToSend);
+      } else {
+         this.logmodule.writelog('debug', "Skip realtime no match found for api topic.");
+      }
+   }
 
    /**
     * updateRef - updates references to other classes
