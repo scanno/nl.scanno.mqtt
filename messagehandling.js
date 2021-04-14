388848bb16806e3885a526d36e81ef11c31886f4
Harrie de Groot <harriedegroot@gmail.com>
2021-04-14 02:55:54 +0200

    clean-up

--- messagehandling.js
+++ messagehandling.js
@@ -29,38 +29,9 @@ class handlingMQTT {
       this.logmodule.writelog('info', "received '" + buffer.toString() + "' on '" + topic + "'");
 
       const message = buffer.toString();
-      //await this.callTriggers(topic, message)
       this.triggerQueue.add(topic, message);
       this.dispatchToRealtimeApi(topic, message);
    }
-
-   // async callTriggers(topic, message){
-   //    let tokens = {
-   //       message: message,
-   //       topic: topic
-   //    }
-
-   //    let triggerstate = {
-   //       triggerTopic: topic,
-   //    }
-
-   //    // pre-check to prevent uneccesairy calls to the Homey trigger system
-   //    // prevents disabling of random flows not even listening to these topics (Homey BUG?)...
-   //    // this is also a performance boost, since the Homey trigger system won't be called for all api topics
-   //    // the small downside is the duplicate topic matching, if there is a match on a trigger topic (here & at the trigger listener...)
-   //    let triggerTopics = this.broker.getTopicArray().getTriggerTopics();
-   //    if(!triggerTopics.some(wildcard => topicMatches(topic, wildcard))) {
-   //       this.logmodule.writelog('debug', "Skip trigger no match found for trigger topic.");
-   //       return;
-   //    }
-
-   //    try {
-   //       this.logmodule.writelog('debug', "Trigger generic card for " + topic);
-   //       await this.triggers.getEventMQTT().trigger(tokens, triggerstate);
-   //    } catch(e) {
-   //       this.logmodule.writelog('error', "Error occured: " +e);
-   //    }
-   // }
 
    dispatchToRealtimeApi(topic, message) {
       var messageToSend;
